c6633ad6f1a507f0579c813ef11128c3e790cb0c
Haoqi Zhang <hqz@mit.edu>
2016-02-22 23:36:22 -0600

    adding entities file (no longer gitignored, since it's a test file anyway)

---
+++ server/entities.js
@@ -0,0 +1,281 @@
+entities=[
+    {
+        "_id": "test001",
+        "active": true,
+        "title": "Test Paper 1: How to write a good paper",
+        "decision": "R1_REVISE",
+        "abstract": "Abstract for test paper 1 goes here.",
+        "authors": [
+            {
+                "id": "auth00001",
+                "type": "author",
+                "venue": "paper",
+                "rank": 1,
+                "givenName": "John",
+                "middleInitial": "",
+                "familyName": "Smith",
+                "email": "johnsmith@test.email",
+                "primary": {
+                    "dept": "Computer Science",
+                    "institution": "University of Testing",
+                    "city": "Test City",
+                    "country": "USA"
+                }
+            },
+            {
+                "id": "auth00002",
+                "type": "author",
+                "venue": "paper",
+                "rank": 1,
+                "givenName": "Jane",
+                "middleInitial": "",
+                "familyName": "Smith",
+                "email": "janesmith@test.email",
+                "primary": {
+                    "dept": "Computer Science",
+                    "institution": "University of Testing",
+                    "city": "Test City",
+                    "country": "USA"
+                }
+            }
+        ],
+        "contactEmail": "johnsmith@test.email",
+        "contactFirstName": "John",
+        "contactLastName": "Smith",
+        "keywords": "Crowdsourcing",
+        "keywordList": [
+	    "Crowdsourcing"
+        ],
+        "paradigm": "social computing",
+        "venue": "paper",
+        "subtype": "paper",
+        "sessions": [],
+        "inSession": false,
+        "authorNames": "John Smith;Jane Smith",
+        "authorAffiliations": "Department of Computer Science"
+    },
+
+   {
+        "_id": "test002",
+        "active": true,
+        "title": "Test Paper 2: How to write a good paper",
+        "decision": "R1_REVISE",
+        "abstract": "Abstract for test paper 2 goes here.",
+        "authors": [
+            {
+                "id": "auth00001",
+                "type": "author",
+                "venue": "paper",
+                "rank": 1,
+                "givenName": "John",
+                "middleInitial": "",
+                "familyName": "Smith",
+                "email": "johnsmith@test.email",
+                "primary": {
+                    "dept": "Computer Science",
+                    "institution": "University of Testing",
+                    "city": "Test City",
+                    "country": "USA"
+                }
+            },
+            {
+                "id": "auth00002",
+                "type": "author",
+                "venue": "paper",
+                "rank": 1,
+                "givenName": "Jane",
+                "middleInitial": "",
+                "familyName": "Smith",
+                "email": "janesmith@test.email",
+                "primary": {
+                    "dept": "Computer Science",
+                    "institution": "University of Testing",
+                    "city": "Test City",
+                    "country": "USA"
+                }
+            }
+        ],
+        "contactEmail": "johnsmith@test.email",
+        "contactFirstName": "John",
+        "contactLastName": "Smith",
+        "keywords": "Social Computing; Crowdsourcing", 
+        "keywordList": [
+            "Social Computing",
+	    "Crowdsourcing"
+        ],
+        "paradigm": "social computing",
+        "venue": "paper",
+        "subtype": "paper",
+        "sessions": [],
+        "inSession": false,
+        "authorNames": "John Smith;Jane Smith",
+        "authorAffiliations": "Department of Computer Science"
+    },
+    {
+	"_id": "test003",
+        "active": true,
+        "title": "Test Paper 3: How to write a good paper",
+        "decision": "R1_REVISE",
+        "abstract": "Abstract for test paper 3 goes here.",
+        "authors": [
+            {
+                "id": "auth00001",
+                "type": "author",
+                "venue": "paper",
+                "rank": 1,
+                "givenName": "John",
+                "middleInitial": "",
+                "familyName": "Smith",
+                "email": "johnsmith@test.email",
+                "primary": {
+                    "dept": "Computer Science",
+                    "institution": "University of Testing",
+                    "city": "Test City",
+                    "country": "USA"
+                }
+            },
+            {
+                "id": "auth00002",
+                "type": "author",
+                "venue": "paper",
+                "rank": 1,
+                "givenName": "Jane",
+                "middleInitial": "",
+                "familyName": "Smith",
+                "email": "janesmith@test.email",
+                "primary": {
+                    "dept": "Computer Science",
+                    "institution": "University of Testing",
+                    "city": "Test City",
+                    "country": "USA"
+                }
+            }
+        ],
+        "contactEmail": "johnsmith@test.email",
+        "contactFirstName": "John",
+        "contactLastName": "Smith",
+        "keywords": "Social Computing",
+        "keywordList": [
+            "Social Computing",
+        ],
+        "paradigm": "social computing",
+        "venue": "paper",
+        "subtype": "paper",
+        "sessions": [],
+        "inSession": false,
+        "authorNames": "John Smith;Jane Smith",
+        "authorAffiliations": "Department of Computer Science"
+    },
+    {
+	"_id": "test004",
+        "active": true,
+        "title": "Test Paper 4: How to write a good paper",
+        "decision": "R1_REVISE",
+        "abstract": "Abstract for test paper 4 goes here.",
+        "authors": [
+            {
+                "id": "auth00001",
+                "type": "author",
+                "venue": "paper",
+                "rank": 1,
+                "givenName": "John",
+                "middleInitial": "",
+                "familyName": "Smith",
+                "email": "johnsmith@test.email",
+                "primary": {
+                    "dept": "Computer Science",
+                    "institution": "University of Testing",
+                    "city": "Test City",
+                    "country": "USA"
+                }
+            },
+            {
+                "id": "auth00002",
+                "type": "author",
+                "venue": "paper",
+                "rank": 1,
+                "givenName": "Jane",
+                "middleInitial": "",
+                "familyName": "Smith",
+                "email": "janesmith@test.email",
+                "primary": {
+                    "dept": "Computer Science",
+                    "institution": "University of Testing",
+                    "city": "Test City",
+                    "country": "USA"
+                }
+            }
+        ],
+        "contactEmail": "johnsmith@test.email",
+        "contactFirstName": "John",
+        "contactLastName": "Smith",
+        "keywords": "Social Computing",
+        "keywordList": [
+            "Social Computing",
+        ],
+        "paradigm": "social computing",
+        "venue": "paper",
+        "subtype": "paper",
+        "sessions": [],
+        "inSession": false,
+        "authorNames": "John Smith;Jane Smith",
+        "authorAffiliations": "Department of Computer Science"
+    },
+    {
+	"_id": "test005",
+        "active": true,
+        "title": "Test Paper 5: How to write a good paper",
+        "decision": "R1_REVISE",
+        "abstract": "Abstract for test paper 5 goes here.",
+        "authors": [
+            {
+                "id": "auth00001",
+                "type": "author",
+                "venue": "paper",
+                "rank": 1,
+                "givenName": "John",
+                "middleInitial": "",
+                "familyName": "Smith",
+                "email": "johnsmith@test.email",
+                "primary": {
+                    "dept": "Computer Science",
+                    "institution": "University of Testing",
+                    "city": "Test City",
+                    "country": "USA"
+                }
+            },
+            {
+                "id": "auth00002",
+                "type": "author",
+                "venue": "paper",
+                "rank": 1,
+                "givenName": "Jane",
+                "middleInitial": "",
+                "familyName": "Smith",
+                "email": "janesmith@test.email",
+                "primary": {
+                    "dept": "Computer Science",
+                    "institution": "University of Testing",
+                    "city": "Test City",
+                    "country": "USA"
+                }
+            }
+        ],
+        "contactEmail": "johnsmith@test.email",
+        "contactFirstName": "John",
+        "contactLastName": "Smith",
+        "keywords": "Social Computing",
+        "keywordList": [
+            "Social Computing",
+        ],
+        "paradigm": "social computing",
+        "venue": "paper",
+        "subtype": "paper",
+        "sessions": [],
+        "inSession": false,
+        "authorNames": "John Smith;Jane Smith",
+        "authorAffiliations": "Department of Computer Science"
+    }
+
+
+]
